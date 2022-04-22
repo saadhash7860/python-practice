211fca60fcdbbedfc2c6fbb692f268f6a9e8833b
saadhash7860 <102537005+saadhash7860@users.noreply.github.com>
2022-04-22 03:35:11 -0700

    Add files via upload

---
+++ python_string_slicing_22nd_march_2022.py
@@ -0,0 +1,42 @@
+##var="dhoni"
+##print(var[0])
+##print(var[-5])
+
+#d - 0,-5
+#h - 1,-4
+#0 - 2,-3
+#n - 3,-2
+#i  - 4,-1
+
+var ="dhoni is csk captain"#string slicing cannot happen on reverse direction
+##print(var[0])
+##print(var[0:])
+##print(var[0:5])
+
+##print(var[6:2])
+
+##string slicing
+var = "Netzwerk Academy"
+##print(var)
+##print(var[0:7])
+'''here in line no 19,20,21 if we run the code it will not print whole netzwerk because[0:7] means 0 is included and 7 is excluded'''
+##print(len(var))
+##print(var[0:17])
+##print(var[ : ]) # here if we run this line it will print whole var bcoz it will start from zero and goes upto end.
+##print(var[0:9:1])
+####print(var[0:9:3]) # here first resolve[0:9]and then skip two characters.
+##print(var[::])#here we can resolve the indexing as [0:17:1] 
+print(var[-1:])
+
+##print(var[::-1])
+##print(var[-1:])
+
+
+
+
+
+
+##s1 = slice(4)
+##s2 = slice(1,5,2)
+##print(string[s1])
+##print(string[s2])
