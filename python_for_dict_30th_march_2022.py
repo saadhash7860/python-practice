7232ece4f369455b39f44e83b6a13d1ae89ae6a5
saadhash7860 <102537005+saadhash7860@users.noreply.github.com>
2022-04-22 03:43:20 -0700

    Add files via upload

---
+++ python_for_dict_30th_march_2022.py
@@ -0,0 +1,46 @@
+##var = {"name":"dhoni","age":33}
+##for x in var:
+##    print(x)
+##
+var = {"name":"dhoni","age":33}
+for x in var.items():
+    print(x)
+
+##var = {"name":"dhoni","age":33}
+##for x in var.values():
+##    print(x)
+
+##my_list = []
+##var = {"name":"dhoni","age":33}
+##for x in var.values():
+##    my_list.append(x)
+##    print(x)
+##print(my_list)
+
+##for x in range(10) :
+##    print(x)
+    
+##for x in range(1,11) :
+##    print(x)
+
+##for x in range(0,10,2) :
+##    print(x)
+
+##var = {}
+##for i in range(5):
+##    var[i] = i*i
+##    if var[i] == 4:
+##        print("found")
+##print(var)
+
+
+##my_list = []
+##for x in range(10):
+##    if x%2 == 0:
+##        my_list.append(x)
+##print(my_list)
+
+##my_list = [x for x in range(10) if x%2 ==0]
+##print(my_list)
+
+
